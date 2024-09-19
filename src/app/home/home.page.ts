f1f81cef3af1c138c7a1b2d732d0241f8cf1198c
unknown <kev34172@gmail.com>
2024-09-19 19:27:25 +0800

    tugas

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  panjang: number = 0; 
+  satuanPanjang: string = ''; 
+  hasilPanjang: string = ''; 
+
+  berat: number = 0; 
+  satuanBerat: string = ''; 
+  hasilBerat: string = ''; 
+
+  suhu: number = 0; 
+  satuanSuhu: string = ''; 
+  hasilSuhu: string = ''; 
+
+  konversiPanjang() {
+    if (this.satuanPanjang === 'km') {
+      this.hasilPanjang = (this.panjang * 1000).toString() + ' m';
+    } else {
+      this.hasilPanjang = (this.panjang / 1000).toString() + ' km';
+    }
+  }
+
+  konversiBerat() {
+    if (this.satuanBerat === 'mg') {
+      this.hasilBerat = (this.berat / 1000).toString() + ' g';
+    } else {
+      this.hasilBerat = (this.berat * 1000).toString() + ' mg';
+    }
+  }
+
+  konversiSuhu() {
+    if (this.satuanSuhu === 'c') {
+      this.hasilSuhu = ((this.suhu * 9 / 5) + 32).toString() + ' F';
+    } else {
+      this.hasilSuhu = ((this.suhu - 32) * 5 / 9).toString() + ' C';
+    }
+  }
+}
